45d1fc59b0fec35a4a0d84cc7e981b7d4c464c70
rain-35 <rainen.school@gmail.com>
2022-10-27 15:00:48 -0600

    started to add one time password inputs

--- App.js
+++ App.js
@@ -5,33 +5,37 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
-
 import { color } from 'react-native-reanimated';
-
-
-
 
 const AppStack = createNativeStackNavigator();
 
+const loggedInStates={
+  NOT_LOGGED_IN: 'NOT_LOGGED_IN',
+  LOGGED_IN: 'LOGGED_IN',
+  CODE_SENT: 'CODE_SENT',
+}
+
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
-  const [isLoggedIn,setIsLoggedIn] = React.useState(false);
+  const [loggedInState,setLoggedInState] = React.useState("NOT_LOGGED_IN");
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [passCode, setPassCode] = React.useState(null);
 
    if (isFirstLaunch == true){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
  
 );
-  }else if(isLoggedIn){
+  }else if(loggedInState == loggedInStates.LOGGED_IN){
     return <Navigation/>
-  }else{
+  }else if (loggedInState == loggedInStates.NOT_LOGGED_IN){
     return(<View>
       <TextInput style={styles.input}
       placeholderTextColor='#211F0F'
-      placeholder='Phone Number'>
+      placeholder='Phone Number'
         value={phoneNumber}
-        onChangeText={setPhoneNumber}
+        onChangeText={setPhoneNumber}>
       </TextInput> 
       <Button
       title='Send'
@@ -42,15 +46,48 @@ return(
           {
             method:'POST',
             headers:{
-              'cpntent-type':'application/text'
+              'content-type':'application/text'
             }
           }
           )
+          setLoggedInState(loggedInStates.CODE_SENT)
         }}
         />
     </View>)
   }
-}
+  else if (loggedInState == loggedInStates.CODE_SENT){
+return(
+  <View>
+    <TextInput style={styles.input}
+      placeholderTextColor='#211F0F'
+      placeholder="add code here"
+        value={passCode}
+        onChangeText={setPassCode}
+        keyboardType = "numeric">
+      </TextInput> 
+      <Button
+      title='Send again'
+        style={styles.button}
+        onPress={async()=>{
+          console.log('login Button was pressed')
+          await fetch('https://dev.stedi.me/twofactorlogin',
+        
+          {
+            method:'POST',
+            headers:{
+              'content-type':'application/text'
+            }
+            body:
+            
+          }
+          )
+          setLoggedInState(loggedInStates.CODE_SENT)
+        }}
+        />
+  </View>
+)
+  }
+  }
  export default App;
 
  const styles = StyleSheet.create({
